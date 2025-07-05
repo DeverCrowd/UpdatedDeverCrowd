7641d6741d01f25c087c38315fdcf532a028b693
Etshino00 <expect54232@gmail.com.com>
2025-07-05 21:49:30 +0300

    edited navbar colored logo

--- frontend/components/Logo.jsx
+++ frontend/components/Logo.jsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 
-const Logo = ({ width, height,className }) => {
+const Logo = ({ width, height, className }) => {
   return (
-    <Image
-      alt="DeverCrowd logo"
-      width={width}
-      height={height}
-      src="/assets/logo.png"
-      className={`${className}`}
-    />
+    <div className={`relative group ${className}`} style={{ width, height }}>
+      {/* الصورة الأساسية */}
+      <Image
+        alt="DeverCrowd logo"
+        src="/assets/logo.png"
+        width={width}
+        height={height}
+        className="absolute top-0 left-0 transition-opacity duration-500 opacity-100 group-hover:opacity-0"
+      />
+
+      {/* الصورة الملونة عند الهوفر */}
+      <Image
+        alt="DeverCrowd logo colored"
+        src="/assets/logo-colored.png"
+        width={width=96}
+        height={height=32}
+        className="absolute top-0 left-0 transition-opacity duration-500 opacity-0 group-hover:opacity-100"
+      />
+    </div>
   );
 };
 
